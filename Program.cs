b4b0d4daba5acb4e7a248095e6e3eb49a07e3d4b
Kiril207 <kirya.sherstyuk.05@mail.ru>
2025-09-21 16:42:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+class DocumentWorker
+{
+    public virtual void EditDocument() {
+        Console.WriteLine("Редактирование документа доступно в версии Pro");
+    }
+
+    public virtual void SaveDocument() {
+        Console.WriteLine("Сохранение документа доступно в версии Pro");
+    }
+
+    public virtual void OpenDocument() {
+        Console.WriteLine("Документ открыт");
+    }
+
+}
+
+class ProDocumentWorker():DocumentWorker 
+{
+    public override void EditDocument()
+    {
+        Console.WriteLine("Документ отредакирован");
+    }
+    public override void SaveDocument()
+    {
+        Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Expert");
+    }
+    public override void OpenDocument()
+    {
+        base.OpenDocument();
+    }
+}
+
+class ExpertDocumentWorker() : ProDocumentWorker
+{
+    public override void SaveDocument()
+    {
+        Console.WriteLine("Документ сохранен в новом формате");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите ключ продукта: ");
+        string key;
+        key = Console.ReadLine();
+        DocumentWorker programm;
+        if (key == "pro")
+        {
+            programm = new ProDocumentWorker();
+        }
+        else if (key == "expert")
+        {
+            programm = new ExpertDocumentWorker();
+        }
+        else
+        {
+            programm = new DocumentWorker();
+        }
+        programm.SaveDocument();
+        programm.OpenDocument();
+        programm.EditDocument();
+    }
+}
